0b125275fc571bdae5c5c68562af614fa3c061b0
Anshu <anshu902956@gmail.com>
2024-04-10 18:55:33 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import authRoutes from "./Routes/auth.routes.js";
 import userRoutes from "./Routes/user.routes.js";
 import messageRoutes from "./Routes/message.routes.js";
@@ -9,12 +10,17 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 app.use(express.json());  //to parse the including requests with JSON payloads (from req.body)
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (rep, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+});
 // app.get("/",(req,res)=>{
 //         //root route http://localhost:5000/
 //         res.send("Server is ready");
